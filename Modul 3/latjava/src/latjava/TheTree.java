3dd5dcf32e11a7ac767fdd1832819c9e8882b340
PieNczW <farrellaydin04@gmail.com>
2023-03-24 23:36:22 +0700

    first

---
+++ Modul 3/latjava/src/latjava/TheTree.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latjava;
+
+/**
+ *
+ * @author HP
+ */
+public class TheTree 
+{
+    public static void main(String[] args) 
+    {
+        //ComperTo and equels
+        String S1,S2;
+        S1 = "abc";
+        S2 = "cde";
+        System.out.println(S2.compareTo(S2));
+        
+        S1 = "abc";
+        S2 = "ABC";
+        System.out.println(S1.equals(S2));
+        System.out.println(S1.equalsIgnoreCase(S2));
+    }
+}
---
+++ Modul 3/latjava/src/latjava/farrellaydinm.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latjava;
+
+/**
+ *
+ * @author HP
+ */
+public class farrellaydinm 
+{
+     public static void main(String[] args) 
+     {
+         System.out.println("Welcome to Java Programming Farrell Aydin Mhasood !!!");
+     }
+}
