25e549fe9fac1f930b60f38183fd5b27c5f3aab9
PieNczW <127370958+PieNczW@users.noreply.github.com>
2023-06-25 15:57:52 +0700

    Add files via upload

---
+++ modul8/main/java/MencetakArgument.java
@@ -0,0 +1,20 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package modul8.main.java;
+/**
+ *
+ * @author HP
+ */
+public class MencetakArgument { 
+    public static void main(String[] args) { 
+                                             
+      for (int i = 0; i < args.length; i++) {
+                                             
+         System.out.println(args[i]);
+      }
+   }
+}
